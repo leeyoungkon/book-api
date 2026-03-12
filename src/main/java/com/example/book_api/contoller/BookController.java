063dcb7f85b81814a8d8822c064cfedc30cfa2cf
leeyoungkon <yklee2002@gmail.com>
2026-03-12 16:33:34 +0900

    2nd commit

--- src/main/java/com/example/book_api/contoller/BookController.java
+++ src/main/java/com/example/book_api/contoller/BookController.java
@@ -1,14 +1,26 @@
 package com.example.book_api.contoller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
-@RestController
+@Controller
 public class BookController {
     
     @GetMapping("/book")
+    @ResponseBody
     public String getBook() {
         // return the title when accessed via /book
         return "도시의 향기";
+    }
+
+    @GetMapping("/search")
+    public String search(@RequestParam("bookName") String bookName, Model model) {
+        // put the parameter into the view model
+        model.addAttribute("bookName", bookName);
+        // view resolver will look for templates/index.html (Thymeleaf)
+        return "index";
     }
 }
